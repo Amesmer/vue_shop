18ea34c8ae5ea050537e943270d20578bd8e6845
Amesmer <535026442@qq.com>
2019-12-26 14:48:12 +0800

    login in & login out complete

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-button type="info" @click="loginout">login out</el-button>
+</template>
+
+<script>
+export default {
+  methods: {
+    loginout() {
+      // 清空token即可实现退出
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
